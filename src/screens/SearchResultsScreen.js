103a49128a31182bd6319cfabc9cfdce8c52c626
Richard Zhang <geteralzx@gmail.com>
2020-05-01 14:14:47 -0500

    search exmaple

--- src/screens/SearchResultsScreen.js
+++ src/screens/SearchResultsScreen.js
@@ -7,4 +7,49 @@ const SearchResultsScreen = () => {
 
 const styles = StyleSheet.create({})
 
+
+
 export default SearchResultsScreen
+
+
+
+
+
+////////////search example///////////////
+
+// ////after load state, 
+// //////insside of render before return///////////////
+// searchCases = (e) => {
+//     let keyword = e.target.value;
+//     this.setState({
+//         search: keyword
+//     })
+// }
+
+
+// if (this.state.loading) {
+//     return (<div>Data Loading</div>)
+// }
+// else
+
+//     console.log("==============app props==============");
+
+// let gData = this.state.allRegions.Global;
+// console.log(gData.TotalConfirmed);
+
+
+
+
+// const countryTable = () => {
+
+//     let regionSet = this.state.allRegions.Countries.filter(region => {
+//         if (this.state.search == null) return this.state.allRegions.Countries
+//         else if (region.Country.toLowerCase().includes(this.state.search.toLowerCase())) return region
+
+//     });
+
+// ///////////inside of return////////////
+
+// <input type="text" placeholder="Enter search term" className="text-warning form-control form-control-sm p-3" onChange={e => this.searchCases(e)} />
+// ////////////////////
+

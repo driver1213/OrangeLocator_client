a7620696570ef9014756db63da502d4278cd9c54
diego rivera <diegora1377@gmail.com>
2020-05-01 12:41:14 -0500

    updated bottom bar nav

--- src/screens/SearchResultsScreen.js
+++ src/screens/SearchResultsScreen.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 
 const SearchResultsScreen = () => {
-    return <Text style={{ fontSize: 48 }}>Tracklist Screen</Text> 
+    return <Text style={{ fontSize: 48 }}>Search Results Screen </Text> 
 }
 
 const styles = StyleSheet.create({})

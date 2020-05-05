e699a0a21e3674152d925554dccd0685e1e7191a
diego rivera <diegora1377@gmail.com>
2020-05-05 03:08:00 -0500

    Login

--- src/actions/locationActions.js
+++ src/actions/locationActions.js
@@ -4,7 +4,7 @@ import data from '../../constants/sampleData.json'
 export const loadAllData = (payload) => ({type: LOAD_DATA, payload});
 
 export const loadData = () => dispatch =>{
-   fetch('http://192.168.31.43:3000/stores')
+   fetch('http://98.195.43.142:3000/stores')
       .then(res => res.json())
       .then(allData => {
         dispatch(loadAllData(allData.data))
